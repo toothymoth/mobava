c0ef75ba85a823db65c5791dc073fbeb1883a5e8
qwikks <84187901+qwikks@users.noreply.github.com>
2023-10-22 12:47:53 +0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,36 +1,32 @@
-
 class Inventory():
     def __init__(self, server, uid):
         self.server = server
         self.uid = uid
         self.expire = 0
-
+    
     def get(self):
         return self.inv
-
+    
     async def add_item(self, name, type_, amount=1):
-        if "_" in name:
-            tid, iid = name.split("_")
-        else:
-            tid = name
-            iid = ""
+        tid = name
+        iid = ""
         redis = self.server.redis
         item = await redis.lrange(f"mob:{self.uid}:items:{name}", 0, -1)
         if item:
             if type_ == "cls":
                 return
             await redis.lset(f"mob:{self.uid}:items:{name}", 1,
-                             int(item[1])+amount)
+                             int(item[1]) + amount)
             for tmp in self.inv["c"][type_]["it"]:
                 if tmp["tid"] == tid and tmp["iid"] == iid:
-                    tmp["c"] = int(item[1])+amount
+                    tmp["c"] = int(item[1]) + amount
                     break
         else:
             await redis.sadd(f"mob:{self.uid}:items", name)
             await redis.rpush(f"mob:{self.uid}:items:{name}", type_, amount)
             type_items = self.inv["c"][type_]["it"]
             type_items.append({"c": amount, "tid": tid, "iid": iid})
-
+    
     async def take_item(self, item, amount=1):
         redis = self.server.redis
         items = await redis.smembers(f"mob:{self.uid}:items")
@@ -60,7 +56,7 @@ class Inventory():
                     type_items.remove(tmp)
                     break
         return True
-
+    
     async def get_item(self, item):
         redis = self.server.redis
         items = await redis.smembers(f"mob:{self.uid}:items")
@@ -68,18 +64,15 @@ class Inventory():
             return 0
         have = int(await redis.lindex(f"mob:{self.uid}:items:{item}", 1))
         return have
-
+    
     async def change_wearing(self, cloth, wearing):
         redis = self.server.redis
         if not await redis.lindex(f"mob:{self.uid}:items:{cloth}", 0):
             not_found = True
         else:
             not_found = False
-        if "_" in cloth:
-            tid, iid = cloth.split("_")
-        else:
-            tid = cloth
-            iid = ""
+        tid = cloth
+        iid = ""
         type_items = self.inv["c"]["cls"]["it"]
         ctp = await redis.get(f"mob:{self.uid}:wearing")
         await self.setclothes(tid)
@@ -100,7 +93,7 @@ class Inventory():
             if not not_found:
                 type_items.append({"c": 1, "iid": iid, "tid": tid})
             await redis.srem(f"mob:{self.uid}:{ctp}", cloth)
-            
+    
     async def setclothes(self, newcloth):
         redis = self.server.redis
         clothes = self.server.clothes
@@ -113,22 +106,18 @@ class Inventory():
                 continue
             if clothes[gender][cloth]["category"] == clothes[gender][newcloth]["category"]:
                 await redis.srem(f"mob:{self.uid}:{ctp}", cloth)
-                if "_" in cloth:
-                    iid = cloth.split("_")[1]
-                    tid = cloth.split("_")[0]
-                else:
-                    iid = ""
-                    tid = cloth
+                iid = ""
+                tid = cloth
                 type_items.append({"c": 1, "iid": iid, "tid": tid})
-
+    
     def __get_expire(self):
         return self.__expire
-
+    
     def __set_expire(self, value):
         self.__expire = value
-
+    
     expire = property(__get_expire, __set_expire)
-
+    
     async def _get_inventory(self):
         self.inv = {"c": {"frn": {"id": "frn", "it": []},
                           "act": {"id": "act", "it": []},
@@ -150,17 +139,11 @@ class Inventory():
             item = items[i]
             if not item:
                 continue
-            if "_" in name:
+            try:
                 self.inv["c"][item[0]]["it"].append({"c": int(item[1]),
-                                                     "iid": name.split("_")[1],
-                                                     "tid": name.split("_")[0]}
-                                                    )
-            else:
-                try:
-                    self.inv["c"][item[0]]["it"].append({"c": int(item[1]),
-                                                         "iid": "",
-                                                         "tid": name})
-                except IndexError:
-                    r = self.server.redis
-                    await r.srem(f"mob:{self.uid}:items", name)
-                    await r.delete(f"mob:{self.uid}:items:{name}")
+                                                     "iid": "",
+                                                     "tid": name})
+            except IndexError:
+                r = self.server.redis
+                await r.srem(f"mob:{self.uid}:items", name)
+                await r.delete(f"mob:{self.uid}:items:{name}")
