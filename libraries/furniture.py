1a63946e1d5f08a94aa0cf041606d48eb44abe8c
qwikks <84187901+qwikks@users.noreply.github.com>
2023-10-22 12:49:27 +0700

    Update furniture.py

--- libraries/furniture.py
+++ libraries/furniture.py
@@ -26,6 +26,7 @@ class Furn(Module):
         redis = self.server.redis
         uid = client.uid
         room = client.room.split("_")[2]
+        item['oid'] = item["lid"]
         await redis.sadd(f"rooms:{uid}:{room}:items",
                          f"{item['tpid']}_{item['oid']}")
         if "rid" in item:
@@ -44,7 +45,12 @@ class Furn(Module):
         uid = client.uid
         room = client.room.split("_")[2]
         items = await redis.smembers(f"rooms:{uid}:{room}:items")
-        item = f"{name['tpid']}_{name['oid']}"
+        try:
+            item = f"{name['tpid']}_{name['lid']}"
+        except KeyError:
+            item = f"{name['tpid']}_{name['oid']}"
+        except TypeError:
+            return
         if item not in items:
             return
         options = await redis.smembers(f"rooms:{uid}:{room}:items:{item}"
@@ -54,6 +60,95 @@ class Furn(Module):
         await redis.delete(f"rooms:{uid}:{room}:items:{item}:options")
         await redis.srem(f"rooms:{uid}:{room}:items", item)
         await redis.delete(f"rooms:{uid}:{room}:items:{item}")
+
+    async def type_add(self, item, client):
+        room = client.room.split("_")
+        redis = self.server.redis
+        uid = client.uid
+        inv = self.server.inv[uid]
+        await inv.take_item(item["tpid"])
+        items = await redis.smembers(f"rooms:{uid}:{room[2]}:items")
+        if any(ext in item["tpid"].lower() for ext in ["wll", "wall"]):
+            walls = []
+            for wall in ["wall", "wll"]:
+                for room_item in items:
+                    if wall in room_item.lower():
+                        furn = await self.server.getFurn(room_item, client)
+                        await self.del_furn(furn, client)
+                        if room_item not in walls:
+                            walls.append(room_item)
+                        if len(walls) == 1:
+                            await inv.add_item("_".join(room_item.split("_")[:-1]), "frn")
+                            add_comfort = self.server.frn["_".join(room_item.split("_")[:-1])]["rating"]
+                            await self.server.redis.decrby(f"mob:{client.uid}:hrt", int(add_comfort or 0))
+            if 'oid' in item:
+                item['lid'] = item['oid']
+                del item['oid']
+            item["x"] = 0.0
+            item["y"] = 0.0
+            item["z"] = 0.0
+            item["d"] = 3
+            await self.add_furn(item, client)
+            item["x"] = 13.0
+            item["d"] = 5
+            if 'lid' in item:
+                item["lid"] += 1
+            await self.add_furn(item, client)
+            add_comfort = self.server.frn[item["tpid"]]["rating"]
+            await self.server.redis.incrby(f"mob:{client.uid}:hrt", int(add_comfort or 0))
+        elif any(ext in item["tpid"].lower() for ext in ["flr", "floor"]):
+            for floor in ["flr", "floor"]:
+                for room_item in items:
+                    if floor in room_item.lower():
+                        furn = await self.server.getFurn(room_item,client)
+                        await self.del_furn(furn, client)
+                        await inv.add_item("_".join(room_item.split("_")[:-1]), "frn")
+                        add_comfort = self.server.frn["_".join(room_item.split("_")[:-1])]["rating"]
+                        await self.server.redis.decrby(f"mob:{client.uid}:hrt", int(add_comfort or 0))
+            item["x"] = 0.0
+            item["y"] = 0.0
+            item["z"] = 0.0
+            item["d"] = 5
+            if 'oid' in item:
+                item['lid'] = item['oid']
+                del item['oid']
+            await self.add_furn(item, client)
+            add_comfort = self.server.frn[item["tpid"]]["rating"]
+            await self.server.redis.incrby(f"mob:{client.uid}:hrt", int(add_comfort or 0))
+
+    async def replace_door(self, item, client):
+        uid = client.uid
+        room = client.room.split("_")
+        redis = self.server.redis
+        inv = self.server.inv[uid]
+        items = await redis.smembers(f"rooms:{uid}:{room[2]}:items")
+        found = None
+        for tmp in items:
+            oid = int(tmp.split("_")[-1])
+            if oid == item["oid"]:
+                found = tmp
+                break
+        if not found:
+            return
+        await inv.take_item(item["tpid"])
+        data = await redis.lrange(f"rooms:{uid}:{room[2]}:items:{found}",
+                                  0, -1)
+        options = await redis.smembers(f"rooms:{uid}:{room[2]}:items:{found}:"
+                                       "options")
+        if "rid" in options:
+            rid = await redis.get(f"rooms:{uid}:{room[2]}:items:{found}:rid")
+        else:
+            return
+        furn = await self.server.getFurn(found, client)
+        await self.del_furn(furn, client)
+        await inv.add_item("_".join(found.split("_")[:-1]), "frn")
+        if "oid" in item:
+            item['lid'] = item['oid']
+            del item['oid']
+        item.update({"x": float(data[0]), "y": float(data[1]),
+                     "z": float(data[2]), "d": int(data[3]),
+                     "rid": rid})
+        await self.add_furn(item, client)
         
     async def save_room(self, msg, client):
         if not self.getOwner(client.room, client.uid):
@@ -62,7 +157,7 @@ class Furn(Module):
         for furn in new_room:
             type_ = furn["t"]
             if type_ == 0:
-                ...
+                await self.type_add(furn, client)
             elif type_ == 1:
                 isNew = True
                 for olditem in await self.server.get_room(client.room):
@@ -82,6 +177,8 @@ class Furn(Module):
                 await self.server.redis.decrby(f"mob:{client.uid}:hrt", int(add_comfort or 0))
                 await self.server.inv[client.uid].add_item(furn["tpid"], "frn")
                 await self.del_furn(furn, client)
+            elif type_ == 3:
+                await self.replace_door(furn, client)
         room_items = {'r': await self.server.get_room(client.room), 'lt': 0}
         cityInfo = await city_info(self.server, client.uid)
         await client.update_inv()
