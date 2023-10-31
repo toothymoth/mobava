34f13f817cccb556795f7c942cf10eda54faa5a3
qwikks <84187901+qwikks@users.noreply.github.com>
2023-10-31 17:11:40 +0700

    Update const.py

--- const.py
+++ const.py
@@ -3,7 +3,7 @@ PORT = 9123
 
 LIBRARIES = ["house", "avatar", "client", "furniture", "outside", 
              "confirm", "relations", "player",
-             "billing", "passport", "inventory", "craft"]
+             "billing", "passport", "inventory", "craft", "event"]
 
 ROOM_COUNT = 7
 
