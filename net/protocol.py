1924cfd483e6a01f1b4f79ba7bf04bee4c924638
qwikks +-+ <84187901+qwikks@users.noreply.github.com>
2023-12-08 16:01:27 +0700

    Update protocol.py
    decode string updated

--- net/protocol.py
+++ net/protocol.py
@@ -1,85 +1,79 @@
+import io
+
+
 class Decoder:
-    def __init__(self, data):
-        self.pos = 0
-        self.data = data
-
-    def read(self, amount):
-        if self.pos + amount > len(self.data):
-            raise Exception("Data is None")
-        old_pos = self.pos
-        self.pos = self.pos + amount
-        return self.data[old_pos:self.pos]
-
+    def __init__(self, bytes_):
+        self.bytes = bytes_
+    
     def decodeByte(self, byte):
-        type, cnt = (">b", 1) if byte == "i8" else \
-                    (">B", 1) if byte == "u8" else \
-                    (">i", 4) if byte == "i32" else \
-                    (">I", 4) if byte == "u32" else \
-                    (">q", 8) if byte == "i64" else \
-                    (">d", 8)
-        return struct.unpack(type, self.read(cnt))[0]
-
+        type_, cnt = (">b", 1)
+        if byte == "u8":
+            type_, cnt = (">B", 1)
+        elif byte == "i16":
+            type_, cnt = (">h", 2)
+        elif byte == "u16":
+            type_, cnt = (">H", 2)
+        elif byte == "i32":
+            type_, cnt = (">i", 4)
+        elif byte == "u32":
+            type_, cnt = (">I", 4)
+        elif byte == "i64":
+            type_, cnt = (">q", 8)
+        elif byte == "f64":
+            type_, cnt = (">d", 8)
+        return struct.unpack(type_, self.bytes.read(cnt))[0]
+    
     def processFrame(self):
-        self.read(9)
+        self.bytes: io.BytesIO = io.BytesIO(self.bytes)
+        self.bytes.read(9)
         return {
-            'type': self.decodeByte('i8'),
+            'type': self.decodeByte("i8"),
             'msg': self.decodeObject()
         }
-
+    
     def decodeValue(self):
-        dataType = self.decodeByte('i8')
-        if dataType == 0: return None
-        elif dataType == 1:
-            return True if self.decodeByte('i8') else False
-        elif dataType == 2: return self.decodeByte('i32')
-        elif dataType == 3: return self.decodeByte('i64')
-        elif dataType == 4: return self.decodeByte('f64')
-        elif dataType == 5: return self.decodeString()
-        elif dataType == 6: return self.decodeDictionary()
-        elif dataType == 7: return self.decodeArray()
-        else: raise ValueError(f"Invalid data type signature = {dataType}")
-
-    def decodeArray(self, element=0):
-        array=[]
-        elements = self.decodeByte('i32')
-        while element < elements:
-            array.append(self.decodeValue()); element += 1
-        return array
-
-    def decodeObject(self, element=0):
-        object={}
-        elements = self.decodeByte('i32')
-        while element < elements:
-            key = self.decodeString()
-            object[key] = self.decodeValue(); element += 1
-        return object
-
-    def decodeDictionary(self, element=0):
-        object={}
-        elements = self.decodeByte('i32')
-        while element < elements:
-            key = self.decodeString()
-            object[key] = self.decodeValue(); element += 1
-        return object
-
-    def decodeString(self):
-        length = 1
-        replaceBytes = self.read(2)
-        while not isinstance(length, str):
-            letter = self.getNextValue(length)
-            if letter:
-                length += 1
-                continue
-            break
-        return self.read(length-1).decode('utf-8', 'ignore')
-
-    def getNextValue(self, element):
-        if not self.data[self.pos+(element-1):self.pos+(element-1)+1]:
+        dataType = self.decodeByte("i8")
+        if dataType == 0:
             return None
-        elements = struct.unpack(">b", self.data[self.pos+(element-1):self.pos+(element-1)+1])[0]
-        if elements > 8 or elements < 0:
-            return self.data[self.pos:self.pos+element]
-        return None
+        elif dataType == 1:
+            return bool(self.decodeByte('i8'))
+        elif dataType == 2:
+            return self.decodeByte('i32')
+        elif dataType == 3:
+            return self.decodeByte('i64')
+        elif dataType == 4:
+            return self.decodeByte('f64')
+        elif dataType == 5:
+            return self.decodeString()
+        elif dataType == 6:
+            return self.decodeObject()
+        elif dataType == 7:
+            return self.decodeArray()
+        else:
+            raise ValueError(f"Invalid data type signature = {dataType}")
+    
+    def decodeArray(self, element=0):
+        array = []
+        elements = self.decodeByte('i32')
+        while element < elements:
+            array.append(self.decodeValue())
+            element += 1
+        return array
+    
+    def decodeObject(self):
+        object = {}
+        elements = self.decodeByte('i32')
+        # print(elements)
+        for _ in range(elements):
+            key = self.decodeString()
+            object[key] = self.decodeValue()
+            # print(key, object[key])
+        return object
+    
+    def decodeString(self):
+        leng: int = self.decodeByte("i16")
+        data = self.bytes.read(leng)
+        return data.decode()
 
 
 import struct
@@ -89,12 +83,12 @@ from datetime import datetime
 class Encoder:
     def __init__(self, data):
         self.data = data
-
+    
     def processFrame(self):
         if isinstance(self.data, dict):
             return self.encodeObject()
         return print(f"Не могу енкодить: {type(self.data)}")
-
+    
     def encodeValue(self, data, forDict=False):
         final_data = b""
         if data is None:
@@ -115,7 +109,7 @@ class Encoder:
         elif isinstance(data, str):
             if not forDict:
                 final_data += struct.pack(">b", 5)
-            length = len(data.encode().hex())//2
+            length = len(data.encode().hex()) // 2
             if len(data) >= 1000:
                 final_data += struct.pack(">b", 4)
             while (length & 4294967168) != 0:
@@ -134,23 +128,23 @@ class Encoder:
             final_data += struct.pack(">b", 8)
             final_data += struct.pack(">q", int(data.timestamp() * 1000))
         else:
-            raise ValueError("Не могу энкодить "+str(type(data)))
+            raise ValueError("Не могу энкодить " + str(type(data)))
         return final_data
-
+    
     def encodeObject(self):
         final_data = struct.pack(">I", len(self.data))
         for item in self.data.keys():
             final_data += self.encodeValue(item, forDict=True)
             final_data += self.encodeValue(self.data[item])
         return final_data
-
+    
     def encodeDict(self, data):
         final_data = struct.pack(">I", len(data))
         for item in data.keys():
             final_data += self.encodeValue(item, forDict=True)
             final_data += self.encodeValue(data[item])
         return final_data
-
+    
     def encodeArray(self, data):
         final_data = struct.pack(">i", len(data))
         for item in data:
